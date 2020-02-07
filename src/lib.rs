0e306d0a640c72094f5334168f23c42b52cf3f84
flyingcanoe <flyingcanoe@protonmail.com>
2020-02-07 15:56:51 -0500

    Commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeMap;
+use std::collections::HashMap;
 use std::collections::binary_heap::BinaryHeap;
 use std::cmp::Ordering;
 use serde::{Serialize, Deserialize};
@@ -6,12 +6,23 @@ use bitvec::prelude::*;
 use unicode_normalization::UnicodeNormalization;
 use bincode;
 use bincode::deserialize;
+use bstr;
+use bstr::{CharOrRaw, BString, ByteVec};
+use bstr::ByteSlice;
+use crate::LocalCharOrRaw::Char;
+use std::mem::align_of;
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+enum LocalCharOrRaw {
+    Char(char),
+    Raw(Box<[u8]>)
+}
 
 
 #[derive(Debug, Clone, Eq, PartialEq)]
 enum NodeKind {
     Internal(Box<Node>, Box<Node>),
-    Leaf(char),
+    Leaf(LocalCharOrRaw),
 }
 
 #[derive(Debug, Clone, Eq, PartialEq)]
@@ -32,31 +43,43 @@ impl PartialOrd for Node {
     }
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
+impl From<CharOrRaw<'_>> for LocalCharOrRaw {
+    fn from(ch: CharOrRaw) -> LocalCharOrRaw {
+        match ch {
+            CharOrRaw::Char(ch) => LocalCharOrRaw::Char(ch),
+            CharOrRaw::Raw(raw_slice) => LocalCharOrRaw::Raw(raw_slice.into()),
+        }
+    }
+}
+
+#[derive(Clone, Serialize, Deserialize, Debug)]
 /// A type containing the necessary information to encode and decode text using huffman code compression.
 /// this struct do not give a code point for every unicode char, instead it only give one to the char
 /// present during the creation of the map.
-pub struct HuffmanCodeMap(BTreeMap<char, BitVec<Lsb0, u8>>);
-
+pub struct HuffmanCodeMap(HashMap<LocalCharOrRaw, BitVec<Lsb0, u8>>);
 
 impl HuffmanCodeMap {
-    ///create a new HuffmanCodeMap witch is optimal for the given string
+    ///create a new HuffmanCodeMap witch is optimal for the given text
+    /// it will treat a slice of byte as mostly valid ut8.
     /// # panic
-    /// panic if the string is zero char wide
-    pub fn new(text: &String) -> Self {
-        let text = text.nfc().collect::<String>();
+    /// panic if the slice is zero byte wide
+    pub fn new(text: &[u8]) -> Self {
         if text.len() == 0 {panic!("Bug zero len wide string cannot be use to create a HuffmanCodeMap")}
         
-        let mut char_list = BTreeMap::new();
-        for ch in text.chars() {
+        let mut char_list = HashMap::new();
+
+        //cont each occurrence of a char or invalid byte
+        for ch in text.chars_or_raws() {
             *char_list.entry(ch).or_insert(0) += 1;
         }
 
         let mut heap = BinaryHeap::new();
+
+        //transform the hashmap into a binary heap of tree node
         for counted_char in char_list {
             heap.push(Node {
                 frequency: counted_char.1,
-                kind: NodeKind::Leaf(counted_char.0),
+                kind: NodeKind::Leaf(counted_char.0.into()),
             });
         }
         
@@ -68,13 +91,13 @@ impl HuffmanCodeMap {
             heap.push(Node::merge(left_chill, right_chill));
         }
 
-        let mut code = HuffmanCodeMap {0: BTreeMap::new()};
+        let mut code = HuffmanCodeMap {0: HashMap::new()};
         generate_codes(&heap.pop().unwrap(), BitVec::new(), &mut code);
     
         code
     }
 
-    fn get_char_code(&self, ch: char) -> Result<BitVec<Lsb0, u8>, ()> {
+    fn get_char_code(&self, ch: LocalCharOrRaw) -> Result<BitVec<Lsb0, u8>, ()> {
         let code_option = self.0.get(&ch).cloned();
         match code_option {
             Some(code) => Result::Ok(code),
@@ -85,45 +108,55 @@ impl HuffmanCodeMap {
 
     ///encode the String using the provided HuffmanCodeMap
     /// return error if there is a char in the String which is not present in the code map
-    pub fn encode(&self, text: String) -> Result<Vec<u8>, ()> {
-        let text = text.nfc().collect::<String>();
-        let char_code_list = text
-            .chars()
-            .map(|ch| { self.get_char_code(ch) });
+    pub fn encode(&self, text: &[u8]) -> Result<Vec<u8>, ()> {
+        let char_list: Vec<CharOrRaw> = text.chars_or_raws().collect();
 
         let mut output_stream: BitVec<Lsb0, u8> = BitVec::new();
-        for vec in char_code_list {
-            output_stream.append(&mut vec?);
+
+        for ch in char_list {
+            output_stream.append(&mut self.get_char_code(ch.into())?)
         }
-        Result::Ok(output_stream.into_vec())
+
+        let len = output_stream.len();
+        let mut output_stream = output_stream.into_vec();
+
+        output_stream.insert(0, 8-(len % 8) as u8);
+
+        Result::Ok(output_stream)
     }
 
-    fn try_get_char_by_code(&self, bitvec: &BitVec) -> Option<char> {
+    fn try_get_char_by_code(&self, bitvec: &BitVec) -> Option<LocalCharOrRaw> {
         let results = self.0.iter().find(|pair| {
             pair.1 == bitvec
         });
 
         match results {
-            Some(tuple) => Some(*tuple.0),
+            Some(tuple) => Some(tuple.0.clone()),
             None => None,
         }
     }
 
     ///decode the string which was encoded with the code map.
     /// if you use the wrong HuffmanCodeMap to decode it will return a incorrect string
-    pub fn decode(&self, bytes_stream: &[u8]) -> String {
-        let mut binary_stream: BitVec<Lsb0, u8> = BitVec::from_slice(bytes_stream);
-        let mut str_cache = String::new();
+    pub fn decode(&self, bytes_stream: &[u8]) -> Vec<u8> {
+        let alignment = *bytes_stream.first().unwrap();
+
+        let (_, bytes_stream) = bytes_stream.split_first().unwrap();
+
+        let mut binary_stream: BitBox<Lsb0, u8> = BitBox::from_slice(bytes_stream);
+        let mut str_cache: Vec<u8>  = vec![];
         let mut char_cache: BitVec = BitVec::new();
 
-        binary_stream.reverse();
-        while !binary_stream.is_empty() {
-            // when the binary_stream is empty it will break
-            // thus the unwrap is fine
-            char_cache.push(binary_stream.pop().unwrap());
-            //binary_stream.remove(0);
+        let binary_stream = &binary_stream[..binary_stream.len() - alignment as usize];
+        
+        for bit in binary_stream.iter() {
+
+            char_cache.push(*bit);
             if let Some(ch) = self.try_get_char_by_code(&char_cache) {
-                str_cache.push(ch);
+                match ch {
+                    LocalCharOrRaw::Char(ch) => str_cache.push_char(ch),
+                    LocalCharOrRaw::Raw(raw) => str_cache.append(&mut raw.to_vec()),
+                }
                 char_cache.clear();
             }
         }
@@ -131,10 +164,12 @@ impl HuffmanCodeMap {
 
     }
 
+
     ///serialize the given HuffmanCodeMap into a Vec<u8> using bincode
     pub fn serialize(&self) -> Result<Vec<u8>, Box<bincode::ErrorKind>> {
         bincode::serialize(self)
     }
+
 
     ///deserialize a slice of u8 into a HuffmanCodeMap.
     ///it should have bean serialize with bincode otherwise it will yield a error
@@ -145,7 +180,7 @@ impl HuffmanCodeMap {
 
 fn generate_codes(node: &Node, prefix: BitVec<Lsb0, u8>, out_codes: &mut HuffmanCodeMap) {
 
-    match node.kind {
+    match &node.kind {
         NodeKind::Internal(ref left_child, ref right_child) => {
             let mut left_prefix = prefix.clone();
             left_prefix.push(false);
@@ -157,7 +192,7 @@ fn generate_codes(node: &Node, prefix: BitVec<Lsb0, u8>, out_codes: &mut Huffman
             generate_codes(&right_child, right_prefix, out_codes);
         }
         NodeKind::Leaf(ch) => {
-            out_codes.0.insert(ch, prefix);
+            out_codes.0.insert(ch.clone(), prefix);
         }
     }
 }
@@ -165,14 +200,14 @@ fn generate_codes(node: &Node, prefix: BitVec<Lsb0, u8>, out_codes: &mut Huffman
 impl Node {
     pub fn merge(node1: Node, node2: Node) -> Node {
         let (bigger_node, smaller_node) = match node1.cmp(&node2) {
-            Ordering::Less => {(node2, node1)},
-            Ordering::Equal => (node1, node2),
-            Ordering::Greater => (node1, node2),
+            Ordering::Less => {(node2, node1.clone())},
+            Ordering::Equal => (node1.clone(), node2),
+            Ordering::Greater => (node1.clone(), node2.clone()),
         };
 
         Node {
             frequency: bigger_node.frequency + smaller_node.frequency,
-            kind: NodeKind::Internal(Box::new(smaller_node), Box::new(bigger_node)),
+            kind: NodeKind::Internal(Box::new(smaller_node.clone()), Box::new(bigger_node.clone())),
         }
     }
 }
@@ -180,19 +215,36 @@ impl Node {
 
 mod test {
     use super::HuffmanCodeMap;
+    use std::fs::File;
+    use std::io::Read;
+
+    #[test]
+    fn invalid_utf8() {
+        let mut file = File::open("invalid_utf8_data").unwrap();
+        let mut buffer: Vec<u8> = vec![];
+        file.read_to_end(&mut buffer).unwrap();
+
+        let map = HuffmanCodeMap::new(&buffer);
+        let mut encoded_data = map.encode(&buffer).unwrap();
+
+        let output_buffer = map.decode(&mut encoded_data);
+
+        assert_eq!(output_buffer, buffer);
+
+    }
 
     #[test]
     fn name() {
         let s = "abbccccdddddeeeeee";
-        let map = HuffmanCodeMap::new(&String::from(s));
-        let code = map.encode(String::from(s));
-        let ss = map.decode( &code.unwrap());
-        assert_eq!(s, ss);
+        let map = HuffmanCodeMap::new(s.as_bytes());
+        let code = map.encode(s.as_bytes());
+        let ss = map.decode( &mut code.unwrap());
+        assert_eq!(s.as_bytes().to_vec(), ss);
     }
 
     #[test]
     #[should_panic]
     fn zero_size_string_for_huffman_code_map() {
-        HuffmanCodeMap::new(&"".into());
+        HuffmanCodeMap::new(&[]);
     }
 }
